d1016b39965a0a9ecb86f15c7a9fe699c2bf221a
Kongou Hikari <hikari@nucleisys.com>
2020-05-19 10:09:05 +0800

    ux600: Ramp-up mmc frequency

--- board/nuclei/hbird/hbird.c
+++ board/nuclei/hbird/hbird.c
@@ -15,9 +15,13 @@
 int board_init(void)
 {
 	/* Set Pinmux to enable QSPI2 for SD boot */
-	writel (0xec000000, 0x10012008);
-	writel (0x10000000, 0x10012004);
-	writel (0xfc030000, 0x10012038);
-	printf ("Hummingbird Platform Initialized\n");
+	writel (0x01, (void *)0x10034000);
+	writel (0xec000000, (void *)0x10012008);
+	writel (0x10000000, (void *)0x10012004);
+	writel (0xfc030000, (void *)0x10012038);
+	writel (0x00, (void *)0x10034000); /* NUSPI Prescaler = 4 */
+	__asm__ __volatile__ ("fence w,o" : : : "memory");
+
+	printf ("Board: Initialized\n");
 	return 0;
 }
